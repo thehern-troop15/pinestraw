71c9e2d83a75d7931a8aef4d1bca8c6755dc6284
The Hern <thehern@gmail.com>
2016-03-01 20:03:27 -0500

    fixed bug

--- views/order/_form.php
+++ views/order/_form.php
@@ -28,7 +28,7 @@ $this->registerJs("$('#number_bales').keyup(function(){
     <?php 
        if (Yii::$app->user->identity->getIsScout())
            print $form->field($model, 'scoutid')->dropDownList($scoutlist)->label('Scout');
-       if (Yii::$app->user->identity->getIsScout())
+       else
            print $form->field($model, 'scoutid')->dropDownList($scoutlist, ['prompt' => 'Choose a Scout'],['options' => [ $model->scoutid => ['selected' => true]]])->label('Scout');
     ?>
 
